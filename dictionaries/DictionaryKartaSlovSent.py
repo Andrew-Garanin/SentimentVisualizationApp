dda4d720bdf4bf0b9f5cf6df9388b7548f1d5702
Andrew-Garanin <andrey-garanin-00@mail.ru>
2022-02-12 20:25:28 +0300

    Moved files to another directory.

--- dictionaries/DictionaryKartaSlovSent.py
+++ dictionaries/DictionaryKartaSlovSent.py
@@ -2,9 +2,9 @@ import pandas as pd
 import spacy
 
 
-class Dictionary:
+class DictionaryKartaSlovSent:
     def __init__(self):
-        self._map_data = pd.read_csv('sentiment_dictionary.csv', sep=';')
+        self._map_data = pd.read_csv('../dictionaries_data/sentiment_dictionary_karta_slov_sent.csv', sep=';')
         self.nlp = spacy.load("ru_core_news_md")
 
     def get_word_tag(self, term):
@@ -14,5 +14,5 @@ class Dictionary:
             print(b[0, 0], b[0, 1])
             return b[0, 1]
         else:
-            print("Ой")
+            print(f"{term} - нет в словаре.")
             return '-'
